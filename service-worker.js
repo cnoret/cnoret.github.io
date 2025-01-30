c55095b3ce710adb6923ae6783d70dc9ed0cb3a8
Christophe Noret <123268925+cnoret@users.noreply.github.com>
2025-01-30 20:26:38 +0100

    Improvements

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.467498538cc13b2a0eebcc7e22ded7d3.js"
+  "/precache-manifest.4e040dd561f2fc04e0b13f6465f4314d.js"
 );
 
 self.addEventListener('message', (event) => {
